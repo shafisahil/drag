acf49aabc8d8d79dd0aedb81836c208e4ac9c944
shafisahil <shafisahil7868@gamil.com>
2021-07-10 04:29:46 -0700

    drag

--- script.js
+++ script.js
@@ -1,34 +1,37 @@
 const fill = document.querySelector('.fill')
 const empties = document.querySelectorAll('.empty')
+ 
 fill.addEventListener('dragstart', dragStart)
 fill.addEventListener('dragend', dragEnd)
 
 for(const empty of empties){
-    fill.addEventListener('dragover', dragOver)
-fill.addEventListener('dragenter', drageEnter)
-fill.addEventListener('dragleave', dragLeave)
-fill.addEventListener('dragdrop', dragDrop)
+empty.addEventListener('dragover', dragOver)
+empty.addEventListener('dragenter', dragEnter)
+empty.addEventListener('dragleave', dragLeave)
+empty.addEventListener('drop', dragDrop)
 }
   
 function dragStart(){
-    this.className += 'hold'
-    setTimeout(() => this.className = 'invisible',0)
-}
+  this.className += ' hold'
+  setTimeout(() => this.className = 'invisible', 0)
+}   
 function dragEnd(){
-    this.className = 'fill'
-    
-}
+        this.className = ' fill'
+} 
 function dragOver(e){
-   e.preventDefault()  
+   e.preventDefault()    
 }
-function drageEnter(e){
+function dragEnter(e){
     e.preventDefault()
-}
+    this.className += 'hovered'
+} 
 function dragLeave(){
-    console.log('drag leave')
+   
+    this.className = 'empty'
 }
 
 function dragDrop(){
+
     this.className = 'empty'
     this.append(fill)
 }
